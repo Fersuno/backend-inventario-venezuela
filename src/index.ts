97dd7610b1a8a4195f90a8e2d88c9bd15f7fdb54
Eduard Coelho <fersunodev@gmail.com>
2022-06-04 16:12:15 -0400

    inicial

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import * as tq from "type-graphql";
+import { ApolloServer } from "apollo-server";
+import { DateTimeResolver } from "graphql-scalars";
+import { context } from "./context";
+import { GraphQLScalarType } from "graphql";
+import { resolvers } from "@generated/type-graphql";
+
+const app = async () => {
+  const schema = await tq.buildSchema({
+    resolvers: resolvers,
+    scalarsMap: [{ type: GraphQLScalarType, scalar: DateTimeResolver }],
+  });
+
+  new ApolloServer({ schema, context: context }).listen({ port: 4001 }, () =>
+    console.log(`🚀 Server ready at: http://localhost:4001`)
+  );
+};
+
+app();
